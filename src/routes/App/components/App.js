e7ce2411e93a5dcb01c5fa6b5808c0d13d1f74bb
yoho <vnaticzhock@gmail.com>
2022-06-16 18:58:27 +0800

    template

---
+++ src/routes/App/components/App.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import '../styles/_app.scss';
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+  const [faIcon, setFaIcon] = useState({
+    icon: faMoon,
+    color: '#000000'
+  })
+
+  const changeTheme = () => {
+    setDarkMode((prev) => !prev)
+  }
+
+  useEffect(() => {
+    const root = document.getElementById('root')
+    if(darkMode === true) {
+      root.classList.add('dark-mode')
+      setFaIcon({
+        icon: faSun,
+        color: '#FFA500',
+      })
+    } else {
+      root.classList.remove('dark-mode')
+      setFaIcon({
+        icon: faMoon,
+        color: '#000000'
+      })
+    }
+    
+  }, [darkMode])
+
+  return (
+    <div id="background" className="app">
+      <div className="level">
+        <div>
+          <h1 className="title">Dark Mode Challenge</h1>
+        </div>
+
+        {/* --The button that should toggle dark mode-- */}
+        <button className="app__dark-mode-btn icon level-right">
+          <FontAwesomeIcon icon={faIcon.icon} onClick={changeTheme} color={faIcon.color} />
+        </button>
+
+      </div>
+
+      <div className="columns">
+        <div className="column">
+          <p>Lollipop powder powder. Cotton candy caramels chupa chups halvah muffin caramels apple pie topping cake. Topping chocolate bar pastry chocolate cake. Cupcake tart jujubes dragée jelly-o icing sugar plum. Chocolate bar lollipop candy canes. Biscuit croissant apple pie pudding caramels wafer tart tootsie roll macaroon. Croissant tiramisu chocolate bar carrot cake lemon drops halvah.</p>
+        </div>
+        <div className="column">
+          <p>Marshmallow tiramisu liquorice bear claw chocolate bar bear claw tart. Muffin chupa chups pie. Brownie apple pie topping lemon drops marzipan toffee. Pudding macaroon icing ice cream bonbon cake tart. Pudding sugar plum chocolate cake cake biscuit pastry pastry chocolate bar tart. Lemon drops dessert gummies icing.</p>
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Name" />
+        </div>
+      </div>
+
+      <div className="field">
+        <div className="control">
+          <input className="input" type="text" placeholder="Email" />
+        </div>
+      </div>
+
+      <section className="section">
+        <div className="buttons level-right">
+          <a className="button is-primary">Save</a>
+          <a className="button is-link">Submit</a>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
